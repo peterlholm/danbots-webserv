f5a6ec9588aab3b1c3c7bd0bbdfb8989831522c6
Peter Holm <peter@l-holm.dk>
2021-06-03 22:07:03 +0200

    version included

--- setup/update.sh
+++ setup/update.sh
@@ -4,4 +4,7 @@ echo update copy to /var/www
 
 cp -r ../* /var/www/webservice
 
+DATE=`date +%x`
+echo "INSTALLED=\"$DATE\"\n" >/var/www/webservice/webservice/installed.py
+
 systemctl restart webservice.service
---
+++ webservice/version.py
@@ -0,0 +1,7 @@
+#
+# contain the verison information
+#
+from installed import INSTALLED as installed
+
+VERSION = "0.1"
+INSTALLED = installed
