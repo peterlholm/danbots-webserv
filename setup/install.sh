e1a953e91acfef345368b84a7c60c59c00399eaf
Peter Holm <peter@l-holm.dk>
2021-08-06 10:57:04 +0200

    update

--- setup/install.sh
+++ setup/install.sh
@@ -12,9 +12,15 @@ ln -s /var/www/webservice/apache/webservice.conf /etc/apache2/sites-available
 
 
 sudo apt install libapache2-mod-wsgi-py3
+
 sudo apt install pigpiod
+systemctl enable pigpiod.service
+systemctl restart pigpiod.service
 
 pip3 install -r ../requirements.txt
+
+# access to own PWM module
+usermod -aG i2c danwand
 
 #a2ensite webservice
 #systemctl restart apache2
@@ -22,5 +28,8 @@ pip3 install -r ../requirements.txt
 # as system service
 
 cp ../apache/webservice.service /etc/systemd/system/
+
+systemctl start pigpiod.service
+
 systemctl enable webservice.service
 systemctl restart webservice.service
